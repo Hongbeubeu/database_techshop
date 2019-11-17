57ad39dc589f581adaff5ae8a1ceaaa1d575563c
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2019-11-17 16:13:02 +0700

    database techshop

---
+++ techshop_laptop_description.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: techshop
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `laptop_description`
+--
+
+DROP TABLE IF EXISTS `laptop_description`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `laptop_description` (
+  `product_id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `chip` varchar(45) NOT NULL,
+  `ram` varchar(10) NOT NULL,
+  `vga` varchar(45) NOT NULL,
+  `display` varchar(45) NOT NULL,
+  `camera` varchar(45) NOT NULL,
+  `hard_disk` varchar(45) NOT NULL,
+  `keyboard` varchar(45) NOT NULL,
+  `port` varchar(45) NOT NULL,
+  `battery` varchar(45) NOT NULL,
+  `opera_system` varchar(45) NOT NULL,
+  `create_at` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`),
+  CONSTRAINT `fk_latop_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `laptop_description`
+--
+
+LOCK TABLES `laptop_description` WRITE;
+/*!40000 ALTER TABLE `laptop_description` DISABLE KEYS */;
+/*!40000 ALTER TABLE `laptop_description` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-17 16:10:14
